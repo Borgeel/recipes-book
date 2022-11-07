1d95ba05e238b49c3fe04218da8f8b742530f5bf
Borgeel <irfancesir93@gmail.com>
2022-11-07 23:39:14 +0100

    navbar navigation functionality, recipes- select a recipe functionality added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'max-course-basics';
+  targettedLink: string = '';
+
+  onSelect(target: string) {
+    this.targettedLink = target;
+  }
 }
